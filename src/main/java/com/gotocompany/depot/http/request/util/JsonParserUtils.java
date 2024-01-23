8faa937f4f73a47e184f93acd9ba8368ba8bd164
Sumit Aich <aich.1998@gmail.com>
2024-01-23 20:41:22 +0530

    fix: fix check style

--- src/main/java/com/gotocompany/depot/http/request/util/JsonParserUtils.java
+++ src/main/java/com/gotocompany/depot/http/request/util/JsonParserUtils.java
@@ -74,7 +74,7 @@ public class JsonParserUtils {
 
     public static JsonNode parseInternal(ArrayNode arrayNode, ParsedMessage parsedMessage) {
         ArrayNode tempJsonArray = new JsonNodeFactory(false).arrayNode();
-        for (Iterator<JsonNode> it = arrayNode.elements(); it.hasNext(); ) {
+        for (Iterator<JsonNode> it = arrayNode.elements(); it.hasNext();) {
             JsonNode jsonElement1 = it.next();
             JsonNode parsedJsonNode = JsonParserUtils.parse(jsonElement1, parsedMessage);
             tempJsonArray.add(parsedJsonNode);
