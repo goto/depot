91a31bcd46b3fd94b1e9aaf7c0d7ef5db52ef07b
Sumit Aich <aich.1998@gmail.com>
2024-02-20 20:00:35 +0530

    fix: remove default values of oauth configs

--- src/main/java/com/gotocompany/depot/config/HttpClientConfig.java
+++ src/main/java/com/gotocompany/depot/config/HttpClientConfig.java
@@ -16,18 +16,14 @@ public interface HttpClientConfig extends SinkConfig {
     Boolean isHttpOAuth2Enable();
 
     @Key("SINK_HTTPV2_OAUTH2_ACCESS_TOKEN_URL")
-    @DefaultValue("https://localhost:8888")
     String getHttpOAuth2AccessTokenUrl();
 
     @Key("SINK_HTTPV2_OAUTH2_CLIENT_NAME")
-    @DefaultValue("client_name")
     String getHttpOAuth2ClientName();
 
     @Key("SINK_HTTPV2_OAUTH2_CLIENT_SECRET")
-    @DefaultValue("client_secret")
     String getHttpOAuth2ClientSecret();
 
     @Key("SINK_HTTPV2_OAUTH2_SCOPE")
-    @DefaultValue("scope")
     String getHttpOAuth2Scope();
 }
