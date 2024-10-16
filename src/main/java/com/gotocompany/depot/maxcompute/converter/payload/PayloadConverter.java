a62406947ee7e937d036c48de65a2fcc1dd4c44e
Eka Winata <eka.winata@gopay.co.id>
2024-10-16 11:17:06 +0700

    chore: remove payload converter from branch

--- src/main/java/com/gotocompany/depot/maxcompute/converter/payload/PayloadConverter.java
+++
@@ -1,8 +0,0 @@
-package com.gotocompany.depot.maxcompute.converter.payload;
-
-import com.google.protobuf.Descriptors;
-
-public interface PayloadConverter {
-    Object convert(Descriptors.FieldDescriptor fieldDescriptor, Object object);
-    boolean canConvert(Descriptors.FieldDescriptor fieldDescriptor);
-}
