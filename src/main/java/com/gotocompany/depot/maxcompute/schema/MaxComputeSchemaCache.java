cb67c70fcf7812ea0d657735af79d0034f253ca4
Eka Winata <eka.winata@gopay.co.id>
2024-10-21 17:09:13 +0700

    refactor: add MaxComputeSchemaCache refresh implementation

--- src/main/java/com/gotocompany/depot/maxcompute/schema/MaxComputeSchemaCache.java
+++ src/main/java/com/gotocompany/depot/maxcompute/schema/MaxComputeSchemaCache.java
@@ -1,9 +1,48 @@
 package com.gotocompany.depot.maxcompute.schema;
 
+import com.gotocompany.depot.config.SinkConfig;
+import com.gotocompany.depot.maxcompute.helper.MaxComputeSchemaHelper;
 import com.gotocompany.depot.maxcompute.model.MaxComputeSchema;
-import lombok.Data;
+import com.gotocompany.depot.message.SinkConnectorSchemaMessageMode;
+import com.gotocompany.depot.metrics.StatsDReporter;
+import com.gotocompany.depot.stencil.DepotStencilUpdateListener;
+import com.gotocompany.depot.utils.StencilUtils;
+import com.gotocompany.stencil.StencilClientFactory;
+import com.gotocompany.stencil.client.StencilClient;
+import com.gotocompany.stencil.config.StencilConfig;
+import lombok.Getter;
 
-@Data
-public class MaxComputeSchemaCache {
+public class MaxComputeSchemaCache extends DepotStencilUpdateListener {
+
+    private final MaxComputeSchemaHelper maxComputeSchemaHelper;
+    private final SinkConfig sinkConfig;
+    private final StencilClient stencilClient;
+    @Getter
     private MaxComputeSchema maxComputeSchema;
+
+    public MaxComputeSchemaCache(MaxComputeSchemaHelper maxComputeSchemaHelper,
+                                 SinkConfig sinkConfig,
+                                 StatsDReporter statsDReporter) {
+        this.maxComputeSchemaHelper = maxComputeSchemaHelper;
+        this.sinkConfig = sinkConfig;
+        if (sinkConfig.isSchemaRegistryStencilEnable()) {
+            StencilConfig stencilConfig = StencilUtils.getStencilConfig(sinkConfig, statsDReporter.getClient(), this);
+            stencilClient = StencilClientFactory.getClient(sinkConfig.getSchemaRegistryStencilUrls(), stencilConfig);
+        } else {
+            stencilClient = StencilClientFactory.getClient();
+        }
+    }
+
+
+    @Override
+    public void updateSchema() {
+        this.maxComputeSchema = maxComputeSchemaHelper.buildMaxComputeSchema(
+                stencilClient.get(getSchemaClass())
+        );
+    }
+
+    private String getSchemaClass() {
+        return sinkConfig.getSinkConnectorSchemaMessageMode() == SinkConnectorSchemaMessageMode.LOG_MESSAGE
+                ? sinkConfig.getSinkConnectorSchemaProtoMessageClass() : sinkConfig.getSinkConnectorSchemaProtoKeyClass();
+    }
 }
