0a6447e68265c60f7c53f204eb07f08c27a2b0df
Eka Winata <eka.winata@gopay.co.id>
2024-10-21 15:55:48 +0700

    feat: MaxComputeSchemaCache

---
+++ src/main/java/com/gotocompany/depot/maxcompute/schema/MaxComputeSchemaCache.java
@@ -0,0 +1,9 @@
+package com.gotocompany.depot.maxcompute.schema;
+
+import com.gotocompany.depot.maxcompute.model.MaxComputeSchema;
+import lombok.Data;
+
+@Data
+public class MaxComputeSchemaCache {
+    private MaxComputeSchema maxComputeSchema;
+}
