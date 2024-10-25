6c35778c0b1a8043e05057cdcf4d9fbad25469b3
Eka Winata <eka.winata@gopay.co.id>
2024-10-25 14:32:07 +0700

    test: add PartitioningStrategyFactory test

--- src/main/java/com/gotocompany/depot/maxcompute/schema/partition/PartitioningStrategyFactory.java
+++ src/main/java/com/gotocompany/depot/maxcompute/schema/partition/PartitioningStrategyFactory.java
@@ -7,11 +7,25 @@ import com.gotocompany.depot.config.MaxComputeSinkConfig;
 import com.gotocompany.depot.maxcompute.converter.ConverterOrchestrator;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @RequiredArgsConstructor
 public class PartitioningStrategyFactory {
 
     private final ConverterOrchestrator converterOrchestrator;
     private final MaxComputeSinkConfig maxComputeSinkConfig;
+    private static final Set<TypeInfo> allowedPartitionKeyTypeInfo;
+
+    static {
+        allowedPartitionKeyTypeInfo = new HashSet<>();
+        allowedPartitionKeyTypeInfo.add(TypeInfoFactory.TIMESTAMP);
+        allowedPartitionKeyTypeInfo.add(TypeInfoFactory.STRING);
+        allowedPartitionKeyTypeInfo.add(TypeInfoFactory.TINYINT);
+        allowedPartitionKeyTypeInfo.add(TypeInfoFactory.SMALLINT);
+        allowedPartitionKeyTypeInfo.add(TypeInfoFactory.INT);
+        allowedPartitionKeyTypeInfo.add(TypeInfoFactory.BIGINT);
+    }
 
     public PartitioningStrategy createPartitioningStrategy(Descriptors.Descriptor descriptor) {
         if (!maxComputeSinkConfig.isTablePartitioningEnabled()) {
@@ -24,10 +38,17 @@ public class PartitioningStrategyFactory {
             throw new IllegalArgumentException("Partition key not found in the descriptor: " + partitionKey);
         }
         TypeInfo partitionKeyTypeInfo = converterOrchestrator.convert(fieldDescriptor);
+        checkPartitionTypePrecondition(partitionKeyTypeInfo);
         if (TypeInfoFactory.TIMESTAMP.equals(partitionKeyTypeInfo)) {
             return new TimestampPartitioningStrategy(maxComputeSinkConfig);
         } else {
             return new DefaultPartitioningStrategy(partitionKeyTypeInfo, maxComputeSinkConfig);
+        }
+    }
+
+    private void checkPartitionTypePrecondition(TypeInfo typeInfo) {
+        if (!allowedPartitionKeyTypeInfo.contains(typeInfo)) {
+            throw new IllegalArgumentException("Partition key type not supported: " + typeInfo.getTypeName());
         }
     }
 
