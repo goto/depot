248f516cdf5d576e640da7345f99c6b17d68d687
Eka Winata <eka.winata@gopay.co.id>
2025-01-07 13:41:12 +0700

    refactor: extract common logic to validator class

--- src/main/java/com/gotocompany/depot/maxcompute/converter/TimestampNTZProtobufMaxComputeConverter.java
+++ src/main/java/com/gotocompany/depot/maxcompute/converter/TimestampNTZProtobufMaxComputeConverter.java
@@ -5,15 +5,8 @@ import com.aliyun.odps.type.TypeInfoFactory;
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.Message;
 import com.gotocompany.depot.config.MaxComputeSinkConfig;
-import com.gotocompany.depot.exception.InvalidMessageException;
 import com.gotocompany.depot.maxcompute.model.ProtoPayload;
-
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.temporal.TemporalAmount;
+import com.gotocompany.depot.maxcompute.util.LocalDateTimeValidator;
 
 /**
  * Converts protobuf timestamp to LocalDateTime.
@@ -24,28 +17,11 @@ public class TimestampNTZProtobufMaxComputeConverter implements ProtobufMaxCompu
 
     private static final String SECONDS = "seconds";
     private static final String NANOS = "nanos";
-    private static final long DAYS_IN_YEAR = 365;
 
-    private final TemporalAmount maxPastEventTimeDifference;
-    private final TemporalAmount maxFutureEventTimeDifference;
-    private final ZoneId zoneId;
-    private final LocalDateTime validMinTimestamp;
-    private final LocalDateTime validMaxTimestamp;
-    private final boolean isTablePartitioningEnabled;
-    private final String tablePartitionKey;
-    private final int maxPastYearEventTimeDifference;
-    private final int maxFutureYearEventTimeDifference;
+    private final LocalDateTimeValidator localDateTimeValidator;
 
     public TimestampNTZProtobufMaxComputeConverter(MaxComputeSinkConfig maxComputeSinkConfig) {
-        this.maxPastEventTimeDifference = Duration.ofDays(maxComputeSinkConfig.getMaxPastYearEventTimeDifference() * DAYS_IN_YEAR);
-        this.maxFutureEventTimeDifference = Duration.ofDays(maxComputeSinkConfig.getMaxFutureYearEventTimeDifference() * DAYS_IN_YEAR);
-        this.zoneId = maxComputeSinkConfig.getZoneId();
-        this.validMinTimestamp = maxComputeSinkConfig.getValidMinTimestamp();
-        this.validMaxTimestamp = maxComputeSinkConfig.getValidMaxTimestamp();
-        this.isTablePartitioningEnabled = maxComputeSinkConfig.isTablePartitioningEnabled();
-        this.tablePartitionKey = maxComputeSinkConfig.getTablePartitionKey();
-        this.maxPastYearEventTimeDifference = maxComputeSinkConfig.getMaxPastYearEventTimeDifference();
-        this.maxFutureYearEventTimeDifference = maxComputeSinkConfig.getMaxFutureYearEventTimeDifference();
+        this.localDateTimeValidator = new LocalDateTimeValidator(maxComputeSinkConfig);
     }
 
     @Override
@@ -58,39 +34,7 @@ public class TimestampNTZProtobufMaxComputeConverter implements ProtobufMaxCompu
         Message message = (Message) protoPayload.getParsedObject();
         long seconds = (long) message.getField(message.getDescriptorForType().findFieldByName(SECONDS));
         int nanos = (int) message.getField(message.getDescriptorForType().findFieldByName(NANOS));
-        Instant instant = Instant.now();
-        ZoneOffset zoneOffset = zoneId.getRules().getOffset(instant);
-        LocalDateTime localDateTime = LocalDateTime.ofEpochSecond(seconds, nanos, zoneOffset);
-        validateTimestampRange(localDateTime);
-        validateTimestampPartitionKey(protoPayload.getFieldDescriptor().getName(), localDateTime, protoPayload.isRootLevel());
-        return localDateTime;
-    }
-
-    private void validateTimestampRange(LocalDateTime localDateTime) {
-        if (localDateTime.isBefore(validMinTimestamp) || localDateTime.isAfter(validMaxTimestamp)) {
-            throw new InvalidMessageException(String.format("Timestamp %s is out of allowed range range min: %s max: %s",
-                    localDateTime, validMinTimestamp, validMaxTimestamp));
-        }
-    }
-
-    private void validateTimestampPartitionKey(String fieldName, LocalDateTime eventTime, boolean isRootLevel) {
-        if (!isTablePartitioningEnabled) {
-            return;
-        }
-        if (!isRootLevel) {
-            return;
-        }
-        if (fieldName.equals(tablePartitionKey)) {
-            Instant now = Instant.now();
-            Instant eventTimeInstant = eventTime.toInstant(zoneId.getRules().getOffset(now));
-
-            if (now.minus(maxPastEventTimeDifference).isAfter(eventTimeInstant)) {
-                throw new InvalidMessageException(String.format("Timestamp is in the past, you can only stream data within %d year(s) in the past", maxPastYearEventTimeDifference));
-            }
-            if (now.plus(maxFutureEventTimeDifference).isBefore(eventTimeInstant)) {
-                throw new InvalidMessageException(String.format("Timestamp is in the future, you can only stream data within %d year(s) in the future", maxFutureYearEventTimeDifference));
-            }
-        }
+        return localDateTimeValidator.parseAndValidate(seconds, nanos, protoPayload.getFieldDescriptor().getName(), protoPayload.isRootLevel());
     }
 
 }
--- src/main/java/com/gotocompany/depot/maxcompute/converter/TimestampProtobufMaxComputeConverter.java
+++ src/main/java/com/gotocompany/depot/maxcompute/converter/TimestampProtobufMaxComputeConverter.java
@@ -3,18 +3,22 @@ package com.gotocompany.depot.maxcompute.converter;
 import com.aliyun.odps.type.TypeInfo;
 import com.aliyun.odps.type.TypeInfoFactory;
 import com.google.protobuf.Descriptors;
+import com.google.protobuf.Message;
 import com.gotocompany.depot.config.MaxComputeSinkConfig;
 import com.gotocompany.depot.maxcompute.model.ProtoPayload;
+import com.gotocompany.depot.maxcompute.util.LocalDateTimeValidator;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 public class TimestampProtobufMaxComputeConverter implements ProtobufMaxComputeConverter {
 
-    private final TimestampNTZProtobufMaxComputeConverter timestampNtzProtobufMaxComputeConverter;
+    private static final String SECONDS = "seconds";
+    private static final String NANOS = "nanos";
+
+    private final LocalDateTimeValidator localDateTimeValidator;
 
     public TimestampProtobufMaxComputeConverter(MaxComputeSinkConfig maxComputeSinkConfig) {
-        this.timestampNtzProtobufMaxComputeConverter = new TimestampNTZProtobufMaxComputeConverter(maxComputeSinkConfig);
+        this.localDateTimeValidator = new LocalDateTimeValidator(maxComputeSinkConfig);
     }
 
     @Override
@@ -24,7 +28,10 @@ public class TimestampProtobufMaxComputeConverter implements ProtobufMaxComputeC
 
     @Override
     public Object convertSingularPayload(ProtoPayload protoPayload) {
-        return Timestamp.valueOf((LocalDateTime) timestampNtzProtobufMaxComputeConverter.convertSingularPayload(protoPayload));
+        Message message = (Message) protoPayload.getParsedObject();
+        long seconds = (long) message.getField(message.getDescriptorForType().findFieldByName(SECONDS));
+        int nanos = (int) message.getField(message.getDescriptorForType().findFieldByName(NANOS));
+        return Timestamp.valueOf(localDateTimeValidator.parseAndValidate(seconds, nanos, protoPayload.getFieldDescriptor().getName(), protoPayload.isRootLevel()));
     }
 
 }
