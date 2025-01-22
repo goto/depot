c488fde97336e86367db88784d5baf0114c15cfe
Eka Winata <eka.winata@gopay.co.id>
2025-01-22 15:52:53 +0700

    chore: checkstyle

--- src/main/java/com/gotocompany/depot/maxcompute/converter/initializer/PrimitiveProtobufMappingStrategyFactory.java
+++ src/main/java/com/gotocompany/depot/maxcompute/converter/initializer/PrimitiveProtobufMappingStrategyFactory.java
@@ -6,12 +6,12 @@ public class PrimitiveProtobufMappingStrategyFactory {
 
     public static PrimitiveProtobufMappingStrategy createPrimitiveProtobufMappingStrategy(MaxComputeSinkConfig maxComputeSinkConfig) {
         PrimitiveProtobufMappingStrategy basePrimitiveProtobufMappingStrategy = new BasePrimitiveProtobufMappingStrategy();
-        PrimitiveProtobufMappingStrategy integerPrimitiveProtobufMappingStrategy = maxComputeSinkConfig.isUpcastIntegerTypesEnabled() ?
-                new UpcastedIntegerPrimitiveProtobufMappingStrategy() : new IntegerPrimitiveProtobufMappingStrategy();
-        PrimitiveProtobufMappingStrategy floatPrimitiveProtobufMappingStrategy = maxComputeSinkConfig.isCastFloatToDecimalEnabled() ?
-                new DecimalCastedFloatPrimitiveProtobufMappingStrategy(maxComputeSinkConfig) : new FloatPrimitiveProtobufMappingStrategy();
-        PrimitiveProtobufMappingStrategy doublePrimitiveProtobufMappingStrategy = maxComputeSinkConfig.isCastDoubleToDecimalEnabled() ?
-                new DecimalCastedDoublePrimitiveProtobufMappingStrategy(maxComputeSinkConfig) : new DoublePrimitiveProtobufMappingStrategy();
+        PrimitiveProtobufMappingStrategy integerPrimitiveProtobufMappingStrategy = maxComputeSinkConfig.isUpcastIntegerTypesEnabled()
+                ? new UpcastedIntegerPrimitiveProtobufMappingStrategy() : new IntegerPrimitiveProtobufMappingStrategy();
+        PrimitiveProtobufMappingStrategy floatPrimitiveProtobufMappingStrategy = maxComputeSinkConfig.isCastFloatToDecimalEnabled()
+                ? new DecimalCastedFloatPrimitiveProtobufMappingStrategy(maxComputeSinkConfig) : new FloatPrimitiveProtobufMappingStrategy();
+        PrimitiveProtobufMappingStrategy doublePrimitiveProtobufMappingStrategy = maxComputeSinkConfig.isCastDoubleToDecimalEnabled()
+                ? new DecimalCastedDoublePrimitiveProtobufMappingStrategy(maxComputeSinkConfig) : new DoublePrimitiveProtobufMappingStrategy();
 
         return basePrimitiveProtobufMappingStrategy.mergeStrategy(integerPrimitiveProtobufMappingStrategy)
                 .mergeStrategy(floatPrimitiveProtobufMappingStrategy)
