d655d4e08c345982bfd5ead6d13e83fe9d74414e
Deepansh <deepansh.goyal@gojek.com>
2025-07-29 14:55:57 +0530

    fix: check the value in the map if it is null, then populate a metric

--- src/main/java/com/gotocompany/depot/maxcompute/record/ProtoDataColumnRecordDecorator.java
+++ src/main/java/com/gotocompany/depot/maxcompute/record/ProtoDataColumnRecordDecorator.java
@@ -124,7 +124,7 @@ public class ProtoDataColumnRecordDecorator extends RecordDecorator {
         if (partitioningStrategy != null && partitioningStrategy instanceof TimestampPartitioningStrategy) {
             partitionSpec = partitioningStrategy.getPartitionSpec(recordWrapper.getRecord());
         }
-        if(partitionSpec!= null && partitionSpec.isEmpty()){
+        if(partitionSpec != null && partitionSpec.get(partitioningStrategy.getOriginalPartitionColumnName()) == null) {
             instrumentation.incrementCounter(maxComputeMetrics.getMaxComputeMissingPartitionRecrodsMetric(),
                     String.format(MaxComputeMetrics.MAXCOMPUTE_UNKNOWN_FIELD_VALIDATION_TYPE_TAG, "missing_partition"));
         }
